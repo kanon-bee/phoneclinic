4d464aab73a2ac81ad5cbe74717e053f36bd873e
kanon-bee <kanon754@gmail.com>
2022-01-27 17:07:07 +0000

    thinrd deploy

--- src/Pages/Homepage/HeaderIntro/HeaderIntro1/HeaderIntro1.js
+++ src/Pages/Homepage/HeaderIntro/HeaderIntro1/HeaderIntro1.js
@@ -4,7 +4,7 @@ import styles from './HeaderIntro1.module.css';
 const headerIntro1 = () => (
     <div className={styles.HeaderIntro1}>
         <h2><strong>Excellent Services For Repairing Your Broken Gadget</strong></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud</p>
+        <p>We Repair All sorts of smart Devices and We provide the Best services in South East London</p>
         <button>Make Appoinment</button>
     </div>
 )
